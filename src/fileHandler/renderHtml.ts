c38d1ad82b3c2d62f1d614d1754b6bf5788895ce
null <60427892+vierofernando@users.noreply.github.com>
2021-10-22 11:37:31 +0700

    refactor(renderHtml.ts): add types and fixed indentation

--- src/fileHandler/renderHtml.ts
+++ src/fileHandler/renderHtml.ts
@@ -3,31 +3,31 @@ import path from 'path';
 import { GeneratorOptions } from '../generator';
 import { renderPage } from './';
 import getData from '../utils/getData';
-const renderHtml = async(url: string, options: GeneratorOptions) => {
-   
-    const filePath = path.join(options.srcDir, url);
-    
-    const fileR = `${filePath.substring(0, filePath.length - 4)}ejs`;
-    const file = fs.existsSync(fileR) ? fileR : "404";
 
-    if(file == "404") {
-       return "404: Page Not Found";
+const renderHtml = async (url: string, options: GeneratorOptions): string => {
+    const filePath: string = path.join(options.srcDir, url);
+    
+    const fileR: string = `${filePath.substring(0, filePath.length - 4)}ejs`;
+    const file: string = fs.existsSync(fileR) ? fileR : "404";
+
+    if (file === "404") {
+        return "404: Page Not Found";
     }
   
-    let html = await renderPage(file, await getData(options), options);
-        let normalDoc = false;
-        if(html.endsWith("</body></html>")){
-          normalDoc = true;
-          html = html.substring(0, html.length - "</body></html>".length);
-        }
+    let html: string = await renderPage(file, await getData(options), options);
+    let normalDoc: boolean = false;
+    if (html.endsWith("</body></html>")) {
+        normalDoc = true;
+        html = html.substring(0, html.length - "</body></html>".length);
+    }
 
-        html += `<script>var ssgs=new WebSocket("ws://localhost:${options.port}");ssgs.onmessage=function(event){if(event.data==="reload"){window.location.reload()}}</script>`;
+    html += `<script>var ssgs=new WebSocket("ws://localhost:${options.port}");ssgs.onmessage=function(event){if(event.data==="reload"){window.location.reload()}}</script>`;
 
-        if(normalDoc){
-          html += "</body></html>";
-        }
+    if (normalDoc) {
+        html += "</body></html>";
+    }
 
-        return html;
-      };
+    return html;
+};
 
-      export { renderHtml };
+export { renderHtml };
