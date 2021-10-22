c5fc7f7a89c19afd9e9ab44423d1fbb47f2257da
DevAndromeda <46562212+DevAndromeda@users.noreply.github.com>
2021-10-22 22:59:50 +0545

    refactor(FrostWalker): use random var name

--- src/FrostWalker/compiler.ts
+++ src/FrostWalker/compiler.ts
@@ -4,14 +4,19 @@ import { generate as randomNameGen } from "../utils/genName";
 export function compile<T = unknown>(source: string, options?: T) {
     if (!source || typeof source !== "string") return "";
 
-    let randomName = randomNameGen(5);
+    const outputVar = randomNameGen(5), frostVar = randomNameGen(7);
 
     try {
         return new Function(
-            `let frost=${JSON.stringify(options || {})},{${Object.keys(options || {}).join(",")}}=frost,${randomName}=${JSON.stringify(source)
+            `let ${frostVar}=${
+                JSON.stringify(options || {})
+            },{${
+                Object.keys(options || {}).join(",")
+            }}=${frostVar},${outputVar}=${
+                JSON.stringify(source)
                 .replace(/<!--((.|\n)+?)-->/g, "")
                 .replace(/\<frost\>((.|\n)+?)\<\/frost\>/g, '"+($1)+"')
-                .replace(/\<frost embed\>((.|\n)+?)\<\/frost\>/g, `";$1\n${randomName}+="`)};return ${randomName};`
+                .replace(/\<frost embed\>((.|\n)+?)\<\/frost\>/g, `";$1\n${outputVar}+="`)};return ${outputVar};`
         )() as string;
     } catch (err) {
         throw new FrostError(`Error while rendering template:\n${err}`, "FrostRendererError");
