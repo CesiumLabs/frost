368f604191906938f35e2e9249517b43bb5a3cde
null <60427892+vierofernando@users.noreply.github.com>
2021-10-22 12:34:09 +0700

    refactor(getData.ts): fixed indentation

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -2,15 +2,17 @@ import { GeneratorOptions } from '../generator';
 import recursiveReadDir from './recursiveReadDir';
 import fs from 'node:fs';
 import path from 'node:path';
+
 const getData = async(options: GeneratorOptions): Promise<any> => {
-let data = {};
-await recursiveReadDir(options.srcDir, async (filePath: string) => {
-    const file = path.parse(filePath);
-      if(file.ext === `.ejs`){
-       data = JSON.parse(fs.readFileSync(filePath,'utf-8'));
-      }
-  });
-  return data;
+    let data: any = {};
+
+    await recursiveReadDir(options.srcDir, async (filePath: string) => {
+        const file = path.parse(filePath);
+        if (file.ext === '.ejs') {
+            data = JSON.parse(fs.readFileSync(filePath,'utf-8'));
+        }
+    });
+    return data;
 }
 
 export default getData;
