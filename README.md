92cc40392af296ec199326c0bb9dc9397f12971b
rhydderchc <rhydderchc@gmail.com>
2021-10-22 11:57:56 -0500

    meta tweaks & cli help menu

--- README.md
+++ README.md
@@ -1,22 +1,26 @@
-![frost](https://cdn.discordapp.com/attachments/804373281075363840/901126619471691866/FROST.png)
------------------
+## ![frost](https://cdn.discordapp.com/attachments/804373281075363840/901126619471691866/FROST.png)
+
 ## 🚀 A static site generator.
 
 ### Features
 
-- Super lightweight & I mean it.
-- Easy & simple configuration.
-- Custom, easy to understand and fast templating engine.
-- What else could you possibly want?
+-   Super lightweight & I mean it.
+-   Easy & simple configuration.
+-   Custom, easy to understand and fast templating engine.
+-   What else could you possibly want?
 
 ### Installation
 
-- with npm (Preferred)
+-   with npm (Preferred)
+
 ```bash
 npm i -g frostjs
 ```
+
 ### Usage
-- Simple 
+
+-   Simple
+
 ```
 frost --serve
 ```
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+import pico from "picocolors";
 import { FrostGenerator } from "../generator";
 const f = new FrostGenerator();
 f.loadConfigFile();
@@ -11,4 +11,31 @@ if (args == "--serve" || args == "-s") {
         if (optionsArg) f.serve(Number(optionsArg));
         else if (!optionsArg) f.serve(5348);
     } else if (!options) f.serve(5348);
+}
+
+if (args == "--help" || args == "-h") {
+    console.log(`
+
+                           ${pico.cyan(pico.bold("HELP MENU"))}
+
+
+        Frost is a static site generator made by the developers at devsnowflake. 
+        If you have any problems with the package, feel free to open an issue at
+        https://github.com/devsnowflake/frost/issues
+
+        ${pico.magenta("Feel like Contributing?")}
+        https://github.com/devsnowflake/frost/pulls. Open a pull request with your feature or
+        fix. Any pull requests are welcome. 
+         
+        ${pico.magenta("Do you like the module?")}
+        Star it on github, it means a lot.
+        https://github.com/devsnowflake/frost
+
+        ${pico.bold("Commands")}
+        -----------------------------------
+
+        ${pico.cyan("serve")} --serve -s :: Start your website on a local dev server.
+        ${pico.cyan("help")}  --help  -h :: Opens up this help menu.
+        ${pico.cyan("build")} --build -b :: Build Your Application.
+    `);
 }
