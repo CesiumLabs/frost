db7c07aa36808bd988a123409baa84133c48c3f2
null <60427892+vierofernando@users.noreply.github.com>
2021-10-22 12:37:41 +0700

    refactor(recursiveReadDir): fixed indentation and add types

--- src/utils/recursiveReadDir.ts
+++ src/utils/recursiveReadDir.ts
@@ -1,19 +1,20 @@
 import fs from 'node:fs';
 import path from 'node:path';
-const recurseDirectory = async (dir: string, fileCallback?: (filePath: string) => void, directoryCallback?: (dirPath: string) => void): Promise<void> => {
-    for(const file of fs.readdirSync(dir)){
-      const currentPath = path.join(dir, file);
-    
-      if(fs.lstatSync(currentPath).isDirectory()){
-        if(directoryCallback){
-          directoryCallback(currentPath);
-        }
-  
-        recurseDirectory(currentPath, fileCallback, directoryCallback);
-      }else if(fileCallback){
-        fileCallback(currentPath);
-      }
-    }
-  };
 
-  export default recurseDirectory;
+const recurseDirectory = async (dir: string, fileCallback?: (filePath: string) => void, directoryCallback?: (dirPath: string) => void): Promise<void> => {
+    for (const file of fs.readdirSync(dir)) {
+        const currentPath: string = path.join(dir, file);
+    
+        if (fs.lstatSync(currentPath).isDirectory()) {
+            if (directoryCallback) {
+                directoryCallback(currentPath);
+            }
+
+            recurseDirectory(currentPath, fileCallback, directoryCallback);
+        } else if (fileCallback) {
+            fileCallback(currentPath);
+        }
+    }
+};
+
+export default recurseDirectory;
