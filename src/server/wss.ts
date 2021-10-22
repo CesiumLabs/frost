59dcce1c7b6cffabdbf7b8af2169cb186f69c0a0
null <60427892+vierofernando@users.noreply.github.com>
2021-10-22 12:31:49 +0700

    refactor(wss.ts): fix indentation, turn send func to a single line

--- src/server/wss.ts
+++ src/server/wss.ts
@@ -4,14 +4,12 @@ import WebSocket from 'ws';
 let wss: WebSocket.Server;
 
 const start = (port: number): void => {
-    wss = new WebSocket.Server({port});
-  };
-  
-  const send = (message: string): void => {
-    wss.clients.forEach((client) => client.send(message));
-  };
+    wss = new WebSocket.Server({ port });
+}
 
-  export { 
-      start,
-      send
-  }
+const send = (message: string): void => wss.clients.forEach((client) => client.send(message));
+
+export {
+    start,
+    send
+}
