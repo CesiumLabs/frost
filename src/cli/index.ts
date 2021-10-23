8e7ddca01ff7c850686b0584da4ca344842adcc3
rhydderchc <rhydderchc@gmail.com>
2021-10-23 04:35:27 -0500

    feat(cli): send help command on no args | meta tweaks and prettier

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,6 +6,31 @@ f.loadConfigFile();
 const args: string = process.argv[2];
 const options: string = process.argv[3];
 const optionsArg = process.argv[4];
+
+let HELP_RESPONSE = `
+
+${pico.cyan(pico.bold("HELP MENU"))}
+
+
+Frost is a static site generator made by the developers at devsnowflake. 
+If you have any problems with the package, feel free to open an issue at
+https://github.com/devsnowflake/frost/issues
+
+${pico.magenta("Feel like Contributing?")}
+https://github.com/devsnowflake/frost/pulls. Open a pull request with your feature or
+fix. Any pull requests are welcome. 
+
+${pico.magenta("Do you like the module?")}
+Star it on github, it means a lot.
+https://github.com/devsnowflake/frost
+
+${pico.bold("Commands")}
+-----------------------------------
+
+${pico.cyan("serve")} --serve -s :: Start your website on a local dev server.
+${pico.cyan("help")}  --help  -h :: Opens up this help menu.
+${pico.cyan("build")} --build -b :: Build Your Application.
+`;
 if (args == "--serve" || args == "-s") {
     if (options == "--port") {
         if (optionsArg) f.serve(Number(optionsArg));
@@ -14,28 +39,7 @@ if (args == "--serve" || args == "-s") {
 }
 
 if (args == "--help" || args == "-h") {
-    console.log(`
-
-                           ${pico.cyan(pico.bold("HELP MENU"))}
-
-
-        Frost is a static site generator made by the developers at devsnowflake. 
-        If you have any problems with the package, feel free to open an issue at
-        https://github.com/devsnowflake/frost/issues
-
-        ${pico.magenta("Feel like Contributing?")}
-        https://github.com/devsnowflake/frost/pulls. Open a pull request with your feature or
-        fix. Any pull requests are welcome. 
-         
-        ${pico.magenta("Do you like the module?")}
-        Star it on github, it means a lot.
-        https://github.com/devsnowflake/frost
-
-        ${pico.bold("Commands")}
-        -----------------------------------
-
-        ${pico.cyan("serve")} --serve -s :: Start your website on a local dev server.
-        ${pico.cyan("help")}  --help  -h :: Opens up this help menu.
-        ${pico.cyan("build")} --build -b :: Build Your Application.
-    `);
+    console.log(HELP_RESPONSE);
+} else if (!args) {
+    console.log(HELP_RESPONSE);
 }
--- src/engine/compiler.ts
+++ src/engine/compiler.ts
@@ -4,16 +4,12 @@ import { generate as randomNameGen } from "../utils/genName";
 export function compile<T = unknown>(source: string, options?: T) {
     if (!source || typeof source !== "string") return "";
 
-    const outputVar = randomNameGen(5), frostVar = randomNameGen(7);
+    const outputVar = randomNameGen(5),
+        frostVar = randomNameGen(7);
 
     try {
         return new Function(
-            `let ${frostVar}=${
-                JSON.stringify(options || {})
-            },{${
-                Object.keys(options || {}).join(",")
-            }}=${frostVar},${outputVar}=${
-                JSON.stringify(source)
+            `let ${frostVar}=${JSON.stringify(options || {})},{${Object.keys(options || {}).join(",")}}=${frostVar},${outputVar}=${JSON.stringify(source)
                 .replace(/<!--((.|\n)+?)-->/g, "")
                 .replace(/\<frost\>((.|\n)+?)\<\/frost\>/g, '"+($1)+"')
                 .replace(/\<frost embed\>((.|\n)+?)\<\/frost\>/g, `";$1\n${outputVar}+="`)};return ${outputVar};`
